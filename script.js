43edf49a21871393d045bc590087469f7dbe845f
satifextech <satifextech@gmail.com>
2026-01-08 21:01:22 -0600

    Update fetch URL in script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ form.addEventListener("submit", async (e) => {
 
   const data = new FormData(form);
 
-  const response = await fetch("https://script.google.com/macros/s/AKfycbz_uLnijWvBPxxtULEARPythsAbcbvbvwI1MiemyYB2dlC_zoAD2PcbNJ8M4DK6HLwh1g/exec", {
+  const response = await fetch("https://script.google.com/macros/s/AKfycbxECFY_58mQIQqAlOSrRjTU6L6e8X9yy6I0h5CJJzHO_i9iiM7kaBw65eZIU-S5HQndew/exec", {
     method: "POST",
     body: data
   });
