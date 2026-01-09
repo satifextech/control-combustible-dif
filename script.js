68ecbb4af377644afe41d4dc9b97d80ddc5d7e87
satifextech <satifextech@gmail.com>
2026-01-09 00:50:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
+const form = document.getElementById("fuelForm");
+const msg = document.getElementById("msg");
+
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
-  msg.innerText = "Enviando...";
 
-  try {
-    const data = new FormData(form);
+  const data = new URLSearchParams(new FormData(form));
 
-    const response = await fetch(
-      "https://script.google.com/macros/s/AKfycbwp452GL3wUIWuXPHaOnreP17eGyaK7sxcqgTFT1ClCwpRh_RVYrnzlGxx1wxmcQNX7hg/exec",
-      {
-        method: "POST",
-        body: data
-      }
-    );
+  const response = await fetch(
+    "https://script.google.com/macros/s/AKfycbwp452GL3wUIWuXPHaOnreP17eGyaK7sxcqgTFT1ClCwpRh_RVYrnzlGxx1wxmcQNX7hg/exec",
+    {
+      method: "POST",
+      body: data
+    }
+  );
 
-    const text = await response.text();
-    msg.innerText = text;
-    form.reset();
-
-  } catch (err) {
-    msg.innerText = "Error al enviar";
-    console.error(err);
-  }
+  msg.innerText = "Solicitud enviada correctamente";
+  form.reset();
 });
+
+
