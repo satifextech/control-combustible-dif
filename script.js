ba45a2e4df099e6dc248d30df7c7824d310a0370
satifextech <satifextech@gmail.com>
2026-01-08 21:03:05 -0600

    Update form submission logic and response handling

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const form = document.getElementById("fuelForm");
-const msg = document.getElementById("msg");
-
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
+  msg.innerText = "Enviando...";
 
-  const data = new FormData(form);
+  try {
+    const data = new FormData(form);
 
-  const response = await fetch("https://script.google.com/macros/s/AKfycbxECFY_58mQIQqAlOSrRjTU6L6e8X9yy6I0h5CJJzHO_i9iiM7kaBw65eZIU-S5HQndew/exec", {
-    method: "POST",
-    body: data
-  });
+    const response = await fetch(
+      "https://script.google.com/macros/s/AKfycbxlwrOyNw7ZGStHDecGAWPdE25XmGC5bM9cwDw9iC6pVXfoiXwhDoIUJ-A8EDru7OxybQ/exec",
+      {
+        method: "POST",
+        body: data
+      }
+    );
 
-  msg.innerText = "Solicitud enviada correctamente";
-  form.reset();
+    const text = await response.text();
+    msg.innerText = text;
+    form.reset();
+
+  } catch (err) {
+    msg.innerText = "Error al enviar";
+    console.error(err);
+  }
 });
